2cedd61f508f7d7697c3dfe8bf925d9447e9fd40
aleksandrajakubik <aleksandra.jakubik@outlook.com>
2020-09-07 21:05:58 +0200

    Extract Clock component

---
+++ week4/timeboxing/src/components/Clock.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function isLengthEqual1(stringToCheck) {
+    let newString = stringToCheck;
+    return newString.length === 1 ? newString = '0' + newString : newString;
+
+}
+
+function hoursValidation(hour) {
+    if (hour < 0){
+        return String(0);
+    } else if (hour > 23) {
+        return String(23);
+    } else {
+        return String(hour);
+    }
+};
+
+
+function minutesAndSecondsValidation(time) {
+    if (time < 0) {
+        return '00'
+    } else if (time > 59) {
+        return '59'
+    } else {
+        return String(time);
+    }
+};
+
+function milisecondsValidation(miliseconds) {
+    if (miliseconds < 0) {
+        return '000'
+    } else if (miliseconds > 999) {
+        return '999'
+    } else {
+        let newMiliseconds = String(miliseconds);
+        return newMiliseconds.length === 1 ? newMiliseconds = '00' + newMiliseconds : newMiliseconds.length === 2 ? newMiliseconds = '0' + newMiliseconds : newMiliseconds;
+    }
+}
+
+function Clock({ className = "", hours = 0, minutes = 2, seconds = 9, miliseconds = 0}) {
+    let newHours = isLengthEqual1(hoursValidation(hours));
+    let newMinutes = isLengthEqual1(minutesAndSecondsValidation(minutes));
+    let newSeconds = isLengthEqual1(minutesAndSecondsValidation(seconds));
+    let newMiliseconds = milisecondsValidation(miliseconds);
+    return <h2 className = {"Clock " + className}>Time left: {newHours}:{newMinutes}:{newSeconds}.{newMiliseconds}</h2>
+}
+
+export default Clock;
--- week4/timeboxing/src/index.js
+++ week4/timeboxing/src/index.js
@@ -1,44 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { v4 as uuidv4 } from "uuid";
+import Clock from "./components/Clock"
 
-function isLengthEqual1(stringToCheck) {
-    let newString = stringToCheck;
-    return newString.length === 1 ? newString = '0' + newString : newString;
-
-}
-
-function hoursValidation(hour) {
-    if (hour < 0){
-        return String(0);
-    } else if (hour > 23) {
-        return String(23);
-    } else {
-        return String(hour);
-    }
-};
-
-
-function minutesAndSecondsValidation(time) {
-    if (time < 0) {
-        return '00'
-    } else if (time > 59) {
-        return '59'
-    } else {
-        return String(time);
-    }
-};
-
-function milisecondsValidation(miliseconds) {
-    if (miliseconds < 0) {
-        return '000'
-    } else if (miliseconds > 999) {
-        return '999'
-    } else {
-        let newMiliseconds = String(miliseconds);
-        return newMiliseconds.length === 1 ? newMiliseconds = '00' + newMiliseconds : newMiliseconds.length === 2 ? newMiliseconds = '0' + newMiliseconds : newMiliseconds;
-    }
-}
 
 function TimeboxEditor(props) {
     const { 
@@ -75,13 +39,6 @@ function TimeboxEditor(props) {
     )
 }
 
-function Clock({ className = "", hours = 0, minutes = 2, seconds = 9, miliseconds = 0}) {
-    let newHours = isLengthEqual1(hoursValidation(hours));
-    let newMinutes = isLengthEqual1(minutesAndSecondsValidation(minutes));
-    let newSeconds = isLengthEqual1(minutesAndSecondsValidation(seconds));
-    let newMiliseconds = milisecondsValidation(miliseconds);
-    return <h2 className = {"Clock " + className}>Time left: {newHours}:{newMinutes}:{newSeconds}.{newMiliseconds}</h2>
-}
 
 function ProgressBar({ className = "", percent, trackRemaining = true}) {
     if (trackRemaining) {
