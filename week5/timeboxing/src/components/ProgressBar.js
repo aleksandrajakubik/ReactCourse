a2171a6da3f72cf25b13fc97af3010f84f9fae8a
aleksandrajakubik <aleksandra.jakubik@outlook.com>
2020-09-20 19:21:44 +0200

    Added propTypes for color props

--- week5/timeboxing/src/components/ProgressBar.js
+++ week5/timeboxing/src/components/ProgressBar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import classNames from "classnames";
+import PropTypes from "prop-types";
 
-function ProgressBar({ className = "", percent, trackRemaining = false}) {
+function ProgressBar({ className = "", percent, trackRemaining = false, color = null}) {
     let progressClassName = classNames(
         "progress ", 
         className,
@@ -15,5 +16,20 @@ function ProgressBar({ className = "", percent, trackRemaining = false}) {
         <div className={progressClassName} style = {{"--timePassed": `${percent}%`}}></div>
     )
 }
+
+function ProgressBarValidator(props, propName, componentName) {
+    if (typeof props[propName] !== "number") {
+        return new Error(`Invalid prop '${propName}' issued to component '${componentName}'. It has to be a number.`)
+    } if (props[propName] < 0 || props[propName] > 100) {
+        return new Error(`Invalid prop '${propName}' issued to component '${componentName}'. It has to be a value between 0 and 100.`)
+    }
+}
+
+
+ProgressBar.propTypes = {
+    percent: ProgressBarValidator,
+    color: PropTypes.oneOf(['red', 'green', 'blue'])
+}
+
 
 export default ProgressBar;
