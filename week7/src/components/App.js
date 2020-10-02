a0cbfe241b0259333c9d98c0cf4de6beb2eaed5f
aleksandrajakubik <aleksandra.jakubik@outlook.com>
2020-10-02 18:20:20 +0200

    authentication added

--- week7/src/components/App.js
+++ week7/src/components/App.js
@@ -2,18 +2,71 @@ import React from "react";
 import TimeboxList from "./TimeboxList";
 import EditableTimebox from "./EditableTimebox";
 import ErrorBoundary from "./ErrorBoundary";
+import LoginForm from "./LoginForm";
+import AuthenticationAPI from "../api/FetchAuthenticationApi";
+import jwt from "jsonwebtoken";
 
-function App() {
-    return (
-        <div className="App">
-            <ErrorBoundary message = "Something is not working in aplication..">
-                <TimeboxList />
-                <ErrorBoundary message = "Something is not working in EditableTimebox..">
-                    <EditableTimebox />
+class App extends React.Component {
+    state = {
+        accessToken: null,
+        previousLoginAttemptFailed: false
+    }
+
+    isUserLoggedIn() {
+        return !!this.state.accessToken;
+    }
+
+    getUserEmail() {
+        const decodedToken = jwt.decode(this.state.accessToken);
+        return decodedToken.email;
+    }
+
+    handleLoginAttempt = (credentials) => {
+        AuthenticationAPI.login(credentials)
+            .then( ( {accessToken }) => {
+                this.setState({
+                    accessToken,
+                    previousLoginAttemptFailed: false
+                })
+            }).catch( () => {
+                this.setState({
+                    previousLoginAttemptFailed: true
+                })
+            })
+    }
+
+    handleLogout = () => {
+        this.setState({
+            accessToken: null,
+            previousLoginAttemptFailed: false
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <ErrorBoundary message = "Something is not working in aplication..">
+                    {
+                        this.isUserLoggedIn() ? 
+                            <>
+                                <header className="header">
+                                    Hello {this.getUserEmail()}
+                                    <a onClick = {this.handleLogout} className = "header__logout-link" href ="#">Log out</a>
+                                </header>
+                                <TimeboxList />
+                                <ErrorBoundary message = "Something is not working in EditableTimebox..">
+                                    <EditableTimebox />
+                                </ErrorBoundary>
+                            </> : 
+                            <LoginForm 
+                            errorMessage={this.state.previousLoginAttemptFailed ? "Failed to login" : null}
+                                onLoginAttempt={this.handleLoginAttempt}/>
+                    }
+    
                 </ErrorBoundary>
-            </ErrorBoundary>
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 export default App;
