7ac18a9307a54058704ae4cab2b3203aa038416e
aleksandrajakubik <aleksandra.jakubik@outlook.com>
2020-10-08 22:52:13 +0200

    authenticatedapp exported

--- week8/src/components/App.js
+++ week8/src/components/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import TimeboxList from "./TimeboxList";
-import EditableTimebox from "./EditableTimebox";
 import ErrorBoundary from "./ErrorBoundary";
 import LoginForm from "./LoginForm";
 import AuthenticationAPI from "../api/FetchAuthenticationApi";
-import Header from "./Header";
+import AuthenticatedApp from "./AuthenticatedApp";
 
 
 
@@ -47,11 +45,7 @@ class App extends React.Component {
                     {
                         this.isUserLoggedIn() ? 
                             <>
-                                <Header accessToken={this.state.accessToken} onLogout={this.handleLogout}/>
-                                <TimeboxList accessToken={this.state.accessToken}/>
-                                <ErrorBoundary message = "Something is not working in EditableTimebox..">
-                                    <EditableTimebox />
-                                </ErrorBoundary>
+                                <AuthenticatedApp accessToken={this.state.accessToken} onLogout={this.handleLogout}/>
                             </> : 
                             <LoginForm 
                             errorMessage={this.state.previousLoginAttemptFailed ? "Failed to login" : null}
---
+++ week8/src/components/AuthenticatedApp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from "./Header";
+import TimeboxList from "./TimeboxList";
+import EditableTimebox from "./EditableTimebox";
+import ErrorBoundary from "./ErrorBoundary";
+
+function AuthenticatedApp({accessToken, onLogout}) {
+    return (
+        <>
+        <Header accessToken={accessToken} onLogout={onLogout}/>
+        <TimeboxList accessToken={accessToken}/>
+        <ErrorBoundary message = "Something is not working in EditableTimebox..">
+            <EditableTimebox />
+        </ErrorBoundary>
+        </>
+    )
+}
+
+export default AuthenticatedApp;
