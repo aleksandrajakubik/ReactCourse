7d46d17c932e86a407c85923497a25ff7064d886
aleksandrajakubik <aleksandra.jakubik@outlook.com>
2020-10-21 22:27:45 +0200

    TimeboxCreatir as functional component

--- week8/src/components/TimeboxCreator.js
+++ week8/src/components/TimeboxCreator.js
@@ -1,40 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 
-class TimeboxCreator extends React.Component {
-    constructor(props) {
-        super(props);
-        this.formInput = React.createRef();
-    }
+function TimeboxCreator({ onCreate }) {
 
-    handleSubmit = (event) => {
+    const formInput = useRef();
+
+    const handleSubmit = (event) => {
         event.preventDefault();
-        this.props.onCreate({
-            title: this.formInput.current[0].value,
-            totalTimeInMinutes: this.formInput.current[1].value
+        onCreate({
+            title: formInput.current[0].value,
+            totalTimeInMinutes: formInput.current[1].value
         });
-        this.formInput.current[0].value = "";
-        this.formInput.current[1].value = "";
+        formInput.current[0].value = "";
+        formInput.current[1].value = "";
     }
     
-    render() {
-        return (
-            <form onSubmit = {this.handleSubmit} ref = {this.formInput} className="TimeboxCreator">
-                <label>
-                    What are you doing?
-                    <input 
-                        type="text" 
-                    />
-                </label><br /> 
-                <label>
-                    For how many minutes?
-                    <input 
-                        type="text" 
-                    />
-                </label><br />
-                <button>Add timebox</button>
-            </form>
-        )
-    }
+    return (
+        <form onSubmit = {handleSubmit} ref = {formInput} className="TimeboxCreator">
+            <label>
+                What are you doing?
+                <input 
+                    type="text" 
+                />
+            </label><br /> 
+            <label>
+                For how many minutes?
+                <input 
+                    type="text" 
+                />
+            </label><br />
+            <button>Add timebox</button>
+        </form>
+    )
 }
 
 export default TimeboxCreator;
