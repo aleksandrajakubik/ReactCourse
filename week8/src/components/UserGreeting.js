cc0b72f3cd6fab8501acd37709b0604c95bf6ce3
aleksandrajakubik <aleksandra.jakubik@outlook.com>
2020-10-27 22:06:09 +0100

    context with useContext

--- week8/src/components/UserGreeting.js
+++ week8/src/components/UserGreeting.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import jwt from "jsonwebtoken";
 import AuthenticationContext from '../contexts/AuthenticationContext';
 
 
 function UserGreeting(props) {
 
+    const { accessToken } = useContext(AuthenticationContext);
     return (
-        <AuthenticationContext.Consumer>
-        {
-            ({accessToken}) => <>Hello {getUserEmail(accessToken)}</>
-        }
-        </AuthenticationContext.Consumer>
+        <>
+        Hello {getUserEmail(accessToken)}
+        </>
+        
     )
 
 }
